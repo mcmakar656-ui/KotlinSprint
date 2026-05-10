f014cad95acbfb88f9e81d718559cc5d9b42d288
mcmakar056 <mcmakar056@xmail.ru>
2026-05-10 14:25:50 +0300

    Выполнение задачи 3-3

---
+++ src/main/kotlin/KS-3-3.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    var numberOne = 6
+    println("""
+        ${numberOne} x 1 = ${numberOne * 1}
+        ${numberOne} x 2 = ${numberOne * 2}
+        ${numberOne} x 3 = ${numberOne * 3}
+        ${numberOne} x 4 = ${numberOne * 4}
+        ${numberOne} x 5 = ${numberOne * 5}
+        ${numberOne} x 6 = ${numberOne * 6}
+        ${numberOne} x 7 = ${numberOne * 7}
+        ${numberOne} x 8 = ${numberOne * 8}
+        ${numberOne} x 9 = ${numberOne * 9}
+    """.trimIndent())
+}
