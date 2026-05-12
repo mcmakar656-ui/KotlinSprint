6ef8f5b6ef3f786b34735d3ed04ac85cf10483da
mcmakar056 <mcmakar056@xmail.ru>
2026-05-12 19:50:34 +0300

    Выполнение задачи 4-1

--- src/main/kotlin/KS-4-1.kt
+++ src/main/kotlin/KS-4-1.kt
@@ -1,12 +1,10 @@
 const val NUMBERS_OF_TABLES = 13
 fun main() {
 
-    val busyTablesToday = 13
     val busyTablesTomorrow = 9
+    val busyTablesToday = 13
+    println("[Доступность столиков на сегодня: ${busyTablesToday < NUMBERS_OF_TABLES}],\n[Доступность столиков на завтра: ${busyTablesTomorrow < NUMBERS_OF_TABLES}]")
 
-    val tablesAvailableToday = NUMBERS_OF_TABLES - busyTablesToday > 0
-    val tablesAvailableTommorow = NUMBERS_OF_TABLES - busyTablesTomorrow > 0
-    println("[Доступность столиков на сегодня: ${tablesAvailableToday}]\n[Доступность столиков на завтра: ${tablesAvailableTommorow}]")
 
 }
 
