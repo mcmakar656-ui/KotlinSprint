227ce11f4af65bb93b04b93211629619742a10c5
Валерий <mcmakar056@gmail.com>
2026-05-15 18:19:37 +0300

    Выполнение задачи 4-3

--- src/main/kotlin/KS-4-3.kt
+++ src/main/kotlin/KS-4-3.kt
@@ -1,14 +1,12 @@
 const val REQUIRED_HUMIDITY = 20
-const val  WINTER_SEASON = "зима"
+const val WINTER_SEASON = "зима"
 
 fun main() {
-
-    // Параметры
     val isSunny: Boolean = true
     val isTentOpen: Boolean = true
     val airHumidity: Int = 20
     val season: String = "зима"
 
-    val isConditions = isSunny && isTentOpen && airHumidity == REQUIRED_HUMIDITY &&  WINTER_SEASON != "зима"
+    val isConditions = isSunny && isTentOpen && airHumidity == REQUIRED_HUMIDITY && WINTER_SEASON == season
     println("Благоприятные ли условия сейчас для роста бобовых? $isConditions")
 }
