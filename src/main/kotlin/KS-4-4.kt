e4503bd976c8a14c2e625dfd677a4d75c2c6b2d7
mcmakar056 <mcmakar056@xmail.ru>
2026-05-19 18:57:46 +0300

    Выполнение задачи 4-4

--- src/main/kotlin/KS-4-4.kt
+++ src/main/kotlin/KS-4-4.kt
@@ -1,16 +1,11 @@
 fun main() {
-    val dayTrenning = 1
-    val move = 2
-
-    val moveHands = true
-    val moveLeds = false
-    val backHuman = false
-    val pressHuman = true
+    val dayToday = 5
+    val isOddDay = dayToday % 2 == 1
 
     println("""
-        Упражнения для рук: ${dayTrenning % move == 1 && moveHands == true || dayTrenning % move != 1 && !moveHands}
-        Упражнения для ног: ${dayTrenning % move != 1 && !moveLeds || dayTrenning % move == 1 && moveLeds == true}
-        Упражнения для спины: ${dayTrenning % move != 1 && !backHuman || dayTrenning % move == 1 && backHuman == true}
-        Упражнения для пресса: ${dayTrenning % move == 1 && pressHuman == true || dayTrenning % move != 1 && !pressHuman}
+        Упражнения для рук: $isOddDay
+        Упражнения для ног: ${!isOddDay}
+        Упражнения для спины: ${!isOddDay}
+        Упражнения для пресса: $isOddDay
     """.trimIndent())
 }
