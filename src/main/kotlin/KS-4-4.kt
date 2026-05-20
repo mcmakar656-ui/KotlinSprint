7b2107fe7f8a356d040e9025202c53aa62d31aa6
Валерий <mcmakar056@gmail.com>
2026-05-20 17:04:23 +0300

    Выполнение задачи 4-4

--- src/main/kotlin/KS-4-4.kt
+++ src/main/kotlin/KS-4-4.kt
@@ -2,10 +2,13 @@ fun main() {
     val dayToday = 5
     val isOddDay = dayToday % 2 == 1
 
+    val lenLongWord = "Упражнения для пресса:".length
+
+
     println("""
-        Упражнения для рук: $isOddDay
-        Упражнения для ног: ${!isOddDay}
-        Упражнения для спины: ${!isOddDay}
-        Упражнения для пресса: $isOddDay
+        ${"Упражнения для рук:".padEnd(lenLongWord)} ${isOddDay}
+        ${"Упражнения для ног:".padEnd(lenLongWord)} ${!isOddDay}
+        ${"Упражнения для спины:".padEnd(lenLongWord)} ${!isOddDay}
+        ${"Упражнения для пресса:".padEnd(lenLongWord)} ${isOddDay}
     """.trimIndent())
 }
