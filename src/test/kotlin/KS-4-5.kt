50075c3f58af4cb83ce6afb746cca0d20d432856
Валерий <mcmakar056@gmail.com>
2026-05-21 18:48:43 +0300

    Выполнение задачи 4-5

--- src/test/kotlin/KS-4-5.kt
+++ src/test/kotlin/KS-4-5.kt
@@ -6,18 +6,18 @@ const val COUNT_BOX = 50
 
 fun main() {
     // наличие повреждений корпуса (Boolean переменная);
-    val hasDomageShip = readln()?.toBoolean()?: false
+    val hasDamageShip = readln().toBoolean()
 
     // текущий состав экипажа;
-    val countHuman = readln()?.toInt() ?:0
+    val countHuman =  readln()?.toInt() ?:0
 
-    //количество ящиков с провизией на борту;
-    val countBox = readln()?.toInt() ?:0
+    // количество ящиков с провизией на борту;
+    val countBox =  readln()?.toInt() ?:0
 
     // благоприятность метеоусловий (Boolean переменная).
-    val isWeatherGood = readln()?.toBoolean() ?: false
+    val isWeatherGood = readln().toBoolean()
 
-    val isWayTravel = (!hasDomageShip && countHuman in MIN_HUMAN ..MAX_HUMAN && countBox > COUNT_BOX) ||
-            (hasDomageShip && countHuman == MAX_HUMAN && isWeatherGood && countBox >= COUNT_BOX )
+    val isWayTravel = (!hasDamageShip && countHuman in MIN_HUMAN..MAX_HUMAN && countBox > COUNT_BOX) ||
+            (hasDamageShip && countHuman == MAX_HUMAN && isWeatherGood && countBox >= COUNT_BOX)
     println(isWayTravel)
 }
