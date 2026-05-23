84f6341b8d3e63e01e56c0481d25b30a4079ba0c
mcmakar056 <mcmakar056@xmail.ru>
2026-05-23 09:35:59 +0300

    Выполнение задачи 5-1

--- src/main/kotlin/KS-5-1.kt
+++ src/main/kotlin/KS-5-1.kt
@@ -1,10 +1,8 @@
-
 fun main() {
-
 
     val numberOne = (1..100).random()
     val numberTwo = (1..100).random()
-    println("Сложите два числа: $numberOne + $numberTwo")
+    println("Сложите два числа: $numberOne и $numberTwo")
     if (readln().toInt() == numberOne + numberTwo) {
         println("Добро пожаловать!")
     } else {
